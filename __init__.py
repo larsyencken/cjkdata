c2f72450dce5387eb61e5ccd84540a5e67708a8f
Lars Yencken <lljy@csse.unimelb.edu.au>
2009-04-08 14:22:00 +1000

    Turns bundle into a python package.
    --HG--
    rename : dict/ce_cedict => data/dict/ce_cedict
    rename : dict/je_compdic => data/dict/je_compdic
    rename : dict/je_edict => data/dict/je_edict
    rename : dict/je_enamdict => data/dict/je_enamdict
    rename : dict/je_jplaces => data/dict/je_jplaces
    rename : jp_gazetteer => data/jp_gazetteer
    rename : kanjd212 => data/kanjd212
    rename : kanjidic => data/kanjidic
    rename : lists/char/ch_common_char_level1 => data/lists/char/ch_common_char_level1
    rename : lists/char/ch_common_char_level2 => data/lists/char/ch_common_char_level2
    rename : lists/char/ch_gb2312 => data/lists/char/ch_gb2312
    rename : lists/char/ch_gb2312_level1 => data/lists/char/ch_gb2312_level1
    rename : lists/char/jp_common_char_jinmeiyo => data/lists/char/jp_common_char_jinmeiyo
    rename : lists/char/jp_common_char_joyo => data/lists/char/jp_common_char_joyo
    rename : lists/char/jp_full => data/lists/char/jp_full
    rename : lists/char/jp_jinmeiyo_variant => data/lists/char/jp_jinmeiyo_variant
    rename : lists/char/jp_jinmeiyou => data/lists/char/jp_jinmeiyou
    rename : lists/char/jp_jis => data/lists/char/jp_jis
    rename : lists/char/jp_jlpt_2 => data/lists/char/jp_jlpt_2
    rename : lists/char/jp_jlpt_3 => data/lists/char/jp_jlpt_3
    rename : lists/char/jp_jlpt_4 => data/lists/char/jp_jlpt_4
    rename : lists/char/jp_jyouyou => data/lists/char/jp_jyouyou
    rename : lists/char/jp_kanken_10 => data/lists/char/jp_kanken_10
    rename : lists/char/jp_kanken_5 => data/lists/char/jp_kanken_5
    rename : lists/char/jp_kanken_6 => data/lists/char/jp_kanken_6
    rename : lists/char/jp_kanken_7 => data/lists/char/jp_kanken_7
    rename : lists/char/jp_kanken_8 => data/lists/char/jp_kanken_8
    rename : lists/char/jp_kanken_9 => data/lists/char/jp_kanken_9
    rename : lists/char/jp_sjis => data/lists/char/jp_sjis
    rename : lists/word/ch_han_word_list => data/lists/word/ch_han_word_list
    rename : lists/word/ch_han_word_list_conv => data/lists/word/ch_han_word_list_conv
    rename : lists/word/ch_han_word_list_sharedChar => data/lists/word/ch_han_word_list_sharedChar
    rename : lists/word/ch_word_list_cedict => data/lists/word/ch_word_list_cedict
    rename : lists/word/jp_han_word_list => data/lists/word/jp_han_word_list
    rename : lists/word/jp_han_word_list_conv => data/lists/word/jp_han_word_list_conv
    rename : lists/word/jp_han_word_list_sharedChar => data/lists/word/jp_han_word_list_sharedChar
    rename : lists/word/jp_word_list_edict => data/lists/word/jp_word_list_edict
    rename : lists/word/shared_ch_jp => data/lists/word/shared_ch_jp
    rename : lists/word/shared_ch_jpconv => data/lists/word/shared_ch_jpconv
    rename : lists/word/shared_jp_chconv => data/lists/word/shared_jp_chconv
    rename : radkfile => data/radkfile
    rename : tables/gbk_pinyin_table => data/tables/gbk_pinyin_table
    rename : tables/zhuyin_pinyin_conv_table => data/tables/zhuyin_pinyin_conv_table

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+#  __init__.py
+#  cjktools_data
+# 
+#  Created by Lars Yencken on 08-04-2009.
+#  Copyright 2009 Lars Yencken. All rights reserved.
+#
+
+# vim: ts=4 sw=4 sts=4 et tw=78:
+
